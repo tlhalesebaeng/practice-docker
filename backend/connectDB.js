f9be7bc34d81dd9450e2724358ecbcf1c82461f3
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-22 13:23:13 +0200

    avoid replacing the password on the string

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -6,14 +6,8 @@ const connectDB = async () => {
         // get the database URI
         const DATABASE_URI = process.env.DATABASE_URI;
 
-        // replace the password placeholder on the URI with the database password
-        const DB_URI = DATABASE_URI.replace(
-            '<PASSWORD>',
-            process.env.DATABASE_PASSWORD
-        );
-
-        // connect the app to the dabase using mongoose and the new database uri
-        const connection = await mongoose.connect(DB_URI);
+        // connect the app to the dabase using mongoose and the database uri
+        const connection = await mongoose.connect(DATABASE_URI);
 
         // log to show that the app is connected successfully
         if (connection) {
