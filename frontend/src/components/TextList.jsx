f8a7606cadd3bbc0654d4b57c6d96dd2a12011b5
tlhalesebaeng <tlhalesebaeng@gmail.com>
2025-06-22 14:29:10 +0200

    make headers object readable

--- frontend/src/components/TextList.jsx
+++ frontend/src/components/TextList.jsx
@@ -60,9 +60,7 @@ const TextList = () => {
             setLoading(true);
             const response = await fetch(url, {
                 method: 'PATCH',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ value }),
             });
             setLoading(false);
